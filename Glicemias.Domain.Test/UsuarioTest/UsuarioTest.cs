642c959ee9719e780f9a14d66d75bf63056544a5
FerreirinhaJean <jeangabrielferreira1@gmail.com>
2020-12-05 16:47:19 -0300

    Criação dos projetos de teste com Nunit

---
+++ Glicemias.Domain.Test/UsuarioTest/UsuarioTest.cs
@@ -0,0 +1,32 @@
+﻿using GlicemiasBaseDados.Model;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glicemias.Domain.Test.UsuarioTest
+{
+    [TestFixture]
+
+    public class UsuarioTest
+    {
+
+        [Test]
+        public void CriarUsuario()
+        {
+            string Nome = "Jean Gabriel Ferreira";
+            int Idade = 21;
+            string Cpf = "029.002.050-69";
+
+            var usuario = new Usuario(Nome, Idade, Cpf);
+
+            Assert.That(usuario.Nome, Is.EqualTo(Nome));
+            Assert.That(usuario.Idade, Is.EqualTo(Idade));
+            Assert.That(usuario.Cpf, Is.EqualTo(Cpf));
+
+
+        }
+
+
+    }
+}
---
+++ GlicemiasBaseDados/Model/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlicemiasBaseDados.Model
+{
+    public class Usuario
+    {
+        public virtual string Nome { get; set; }
+        public virtual int Idade { get; set; }
+        public virtual string Cpf { get; set; }
+
+        public Usuario(string nome, int idade, string cpf)
+        {
+            Nome = nome;
+            Idade = idade;
+            Cpf = cpf;
+        }
+    }
+}
